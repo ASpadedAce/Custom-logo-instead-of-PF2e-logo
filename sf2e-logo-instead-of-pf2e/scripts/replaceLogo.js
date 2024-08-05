76c3a1c17afc706f4148f98caceb7fc5adcafebd
ASpadedAce <acegrimreaper@gmail.com>
2024-08-05 14:20:41 -0500

    Delete sf2e-logo-instead-of-pf2e directory

--- sf2e-logo-instead-of-pf2e/scripts/replaceLogo.js
+++
@@ -1,9 +0,0 @@
-Hooks.on('renderActorSheet', (app, html, data) => {
-    const pathfinderLogo = html.find('img[src="systems/pf2e/assets/pathfinder_logo.webp"]');
-    const starfinderLogo = "modules/SF2e-logo-instead-of-PF2e/starfinder_logo.webp";
-  
-    if (pathfinderLogo.length) {
-      pathfinderLogo.attr('src', starfinderLogo);
-    }
-  });
-  
